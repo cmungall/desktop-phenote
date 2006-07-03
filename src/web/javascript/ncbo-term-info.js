672dcbdf8e665075fd81f8a80e02c5330bf3dd24
mgibson <mgibson@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2006-07-03 22:46:02 +0000

    added sohel merchants (from dichtybase) ajax web stuff, ive restructured & renamed stuff - and i havent changed the calls in the scripts to be consistent yet - but i just wanted to get it into svn for our meeting this week. this is the beginnings of making a web interface for phenote. currently it displays one term box and does term completion (ajax) and term info on mouseover (also ajax), provided youve hooked up the ontology scripts. sohel hooks up to dichty with perl/dichty-ontology.cgi (previously scripts/ontology.cgi). ncbo (fly/zfin/obd/berkeley) hook up to ontology data via the new phenote servlet that i just checked in.

---
+++ src/web/javascript/ncbo-term-info.js
@@ -0,0 +1,38 @@
+      
+      // This is a variable which defines the path to the ajax request processing script
+      // This can be changed and should point to the scipt which processes the Ajax queries
+      //var url  = '/usr/local/lib/apache/apache-tomcat-5.5.15/webapps/ROOT/ajax/scripts/phenote-ontology.cgi';
+      //var url  = '/phenote-ontology.cgi';
+  var url = '/servlet/PhenoteStub';
+      
+// set_ontology should be called by the above url (at least for dichty it does)
+
+      function set_ontology(ontologyid){
+
+        // for some reason 0 doesnt work
+        ontologyid = 123;
+
+        //alert("phenote-control.js set_ontology called ontId isNan:"+isNaN(ontologyid)+" ontId: "+ontologyid);
+
+        // isNan is "is Not a Number", ontology id has to be a number
+         if ( !isNaN( ontologyid ) ) {      
+            var pars = 'ontologyid=' + ontologyid;
+
+            
+            //alert("phenote-control.js calling Ajax.Updater with "+pars);
+            //this.debug("phenote-control.js calling Ajax.Updater with "+pars);
+
+            // ontologyinfo is the div where the table goes for term info
+            // note: this is a "get" not a post! (term comp is post)
+            var myAjax = new Ajax.Updater('ontologyinfo', url, {method: 'get', parameters: pars, onComplete: document.forms[0].ontologyname.value = '' } );
+           // have to have [1] next to form item because the edit subform and
+           // the main form both have donorid elements
+            if ( document.forms[0].ontologyid.length > 1 ) {
+               document.forms[0].ontologyid[1].value = ontologyid;
+               document.forms[0].ontologyid[2].value = ontologyid;
+            }
+            else {
+               document.forms[0].ontologyid.value = ontologyid;
+            }
+         }
+      }
---
+++ src/web/readme.txt
@@ -0,0 +1,20 @@
+			README
+--------------------------------------------------------------
+
+This is a widget to search ontologies dynamically using Ajax. The
+following describes the various compoents of the widget and how to use it.
+
+Style sheet: /cs/control.js
+This contains all the styles used in the widget
+
+Javascript:
+/js/control.js contains a method to call Ajax to update the Ontology information.
+In this javascript the "var url" should point to the processing script.
+
+/js/prototype contains Ajax libraries from Prototype
+http://prototype.conio.net/
+
+Scripts
+/scripts contains an exmaple of a Ajax request processing script.
+It is written in CGI perl.
+
