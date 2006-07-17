de6fdd9b9c8d70a058910a01d20836edefa2e6e2
sohelm <sohelm@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2006-07-17 22:34:11 +0000

    new version

--- src/web/javascript/term_completer.js
+++ src/web/javascript/term_completer.js
@@ -2,21 +2,21 @@
       // This is a variable which defines the path to the ajax request processing script
       // This can be changed and should point to the script which process the Ajax queries
       // reference script in source forge is at perl/dichty-ontology.cgi
-      var url  = '/db/cgi-bin/ajax_search/ontology.cgi';
+      var url  = '/db/cgi-bin/ajax_search/term_completer.cgi';
       
-      function set_ontology(ontologyid){
-         if ( !isNaN( ontologyid ) ) {      
-            var pars = 'ontologyid=' + ontologyid;
+      function set_ontology_term(termid){
+         if ( !isNaN( termid ) ) {      
+            var pars = 'termid=' + termid;
 
-            var myAjax = new Ajax.Updater('ontologyinfo', url, {method: 'get', parameters: pars, onComplete: document.forms[0].ontologyname.value = '' } );
+            var myAjax = new Ajax.Updater('terminfo', url, {method: 'get', parameters: pars, onComplete: document.forms[0].term.value = '' } );
            // have to have [1] next to form item because the edit subform and
            // the main form both have donorid elements
-            if ( document.forms[0].ontologyid.length > 1 ) {
-               document.forms[0].ontologyid[1].value = ontologyid;
-               document.forms[0].ontologyid[2].value = ontologyid;
+            if ( document.forms[0].termid.length > 1 ) {
+               document.forms[0].termid[1].value = termid;
+               document.forms[0].termid[2].value = termid;
             }
             else {
-               document.forms[0].ontologyid.value = ontologyid;
+               document.forms[0].termid.value = termid;
             }
          }
       
