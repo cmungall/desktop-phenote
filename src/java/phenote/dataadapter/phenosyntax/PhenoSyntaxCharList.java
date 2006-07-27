640fd8d437d1e96ba68a785216ee93be6bbb98ab
mgibson <mgibson@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2006-07-27 18:46:08 +0000

    ant war file creation task to ant for war files for jboss also fixed compile so it doesnt compile test (whether it needs it or not)

--- src/java/phenote/dataadapter/phenosyntax/PhenoSyntaxCharList.java
+++ src/java/phenote/dataadapter/phenosyntax/PhenoSyntaxCharList.java
@@ -3,3 +3,4 @@ package phenote.dataadapter.phenosyntax;
 
 /** not sure if this is needed as separate class. a lit of PhenoSyntaxChar objects
     that wrap CharacterIs */
+class PhenoSyntaxCharList {}
