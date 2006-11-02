d75feb6f63b1ea3281cdc7db87990aa697857d9f
cmpich <cmpich@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2006-11-02 18:20:29 +0000

    added missing class

---
+++ src/java/phenote/servlet/PhenoteWebConfiguration.java
@@ -0,0 +1,30 @@
+package phenote.servlet;
+
+/**
+ * This contains configuration parameters pertaining to the web application only.
+ */
+public class PhenoteWebConfiguration {
+
+  private String webRoot;
+  private static PhenoteWebConfiguration instance;
+
+  private PhenoteWebConfiguration(){
+
+  }
+
+  public static PhenoteWebConfiguration getInstance(){
+    if(instance == null){
+      instance = new PhenoteWebConfiguration();
+    }
+    return instance;
+  }
+
+
+  public String getWebRoot() {
+    return webRoot;
+  }
+
+  public void setWebRoot(String webRoot) {
+    this.webRoot = webRoot;
+  }
+}
