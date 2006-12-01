a3d3a91679f1ab765f851c7b1892ab21fcf44473
kevinschaper <kevinschaper@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2006-12-01 17:49:23 +0000

    this file is *almost* obsolete, the functionality has been moved to phenote-state.js, but it still has a pair of functions redirecting to the phenoteState methods.

--- src/web/javascript/ncbo-term-info.js
+++ src/web/javascript/ncbo-term-info.js
@@ -14,33 +14,14 @@ var url = '/phenote/Phenote/'; // jboss
 // renaming this getTermInfo... from set_ontology,
 // ontologyid -> termId
 
+
 // ontologyName is the name of the ontology (not a term name!)
-function getTermInfo(termId,termName,ontologyName,field) {
-
-  // for some reason 0 doesnt work
-  //termId = 123;
-  
-  //alert("ncbo-term-info.js set_ontology called termId: "+termId+" field "+field);
-  // isNan is "is Not a Number", ontology id has to be a number
-  //if ( !isNaN( termId ) ) { //var pars = 'ontologyid=' + ontologyid;
-  var pars = 'termId='+termId+'&ontologyName='+ontologyName+'&field='+field;
-            
-  //alert("ncbo-term-info.js calling Ajax.Updater with "+pars);
-  //this.debug("ncbo-term-info.js calling Ajax.Updater with "+pars);
-
-  // ontologyinfo is the div where the table goes for term info
-  // note: this is a "get" not a post! (term comp is post)
-  //var myAjax = new Ajax.Updater('termInfo', url, {method: 'get', parameters: pars, onComplete: document.forms[0].qualityInput.value = '' } );
-  // took out wipe out on complete
-  var myAjax = new Ajax.Updater('termInfo', url, {method: 'get', parameters: pars } );
-  //if ( document.forms[0].ontologyid.length > 1 ) { // dont need this
-  //document.forms[0].ontologyid[1].value = termId;
-  //document.forms[0].ontologyid[2].value = termId; } else {
-  document.termForm.termInfoTermId.value = termId; // }
-  document.termForm.termInfoTermName.value = termName;
-  document.termForm.activeField.value = field;
-  //} if isNan taken out
+function getTermInfo(term) {
+  phenoteState.updateTermInfo(term);
 }
 
 
+function useTermInfo() {
+  phenoteState.useTermInfo();
+}
 
