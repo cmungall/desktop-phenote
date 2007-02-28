5881c05f02f8a7b24956a4395fb7b642dbb493e5
Nicole Washington <nicole.l.washington@gmail.com>
2007-02-28 19:14:56 +0000

    edits to termInfo box - making it look a little prettier

--- src/java/phenote/util/HtmlUtil.java
+++ src/java/phenote/util/HtmlUtil.java
@@ -26,7 +26,11 @@ public class HtmlUtil {
     isStandAlone = standAlone;
   }
 
-  public static String termInfo(OBOClass oboClass) {
+    //this is Mark's old termInfo box.  I'll keep it around
+    //just in case we need to go back to it.  note that it might not
+    //work quite right because i've now changed the other functions it
+    //calls.
+  public static String termInfoOld(OBOClass oboClass) {
     if (oboClass == null) {
       System.out.println("null obo class for HtmlUtil.termInfo");
       return ""; // null? exception?
@@ -35,24 +39,69 @@ public class HtmlUtil {
     if (oboClass.isObsolete())
       sb.append("This term is OBSOLETE").append(newLine());
     sb.append(bold("TERM: ")).append(oboClass.getName());
-    sb.append(nl()).append(bold("ID: ")).append(oboClass.getID());
+    sb.append(nl()).append(bold("ID: ")).append(colorFont(oboClass.getID(), "red"));
+
     Set syns = oboClass.getSynonyms();
     for (Iterator it = syns.iterator(); it.hasNext(); ) {
       sb.append(newLine()).append(bold("Synonym: ")).append(it.next());
     }
-    
-    sb.append(nl()).append(nl()).append(bold("PARENTS: "));
-    sb.append(getParentalString(oboClass));
-    sb.append(nl()).append(nl()).append(bold("CHILDREN: "));
-    sb.append(getChildrenString(oboClass));
 
     String definition = oboClass.getDefinition();
     // definition = lineWrap(definition);
     if (definition != null && !definition.equals(""))
       sb.append(nl()).append(nl()).append(bold("Definition: ")).append(definition);
+    
+    sb.append(nl()).append(nl()).append(bold("CHILDREN: "));
+    sb.append(getChildrenString(oboClass));
+
 
     // if (DEBUG) System.out.println(sb);
 
+    return sb.toString();
+  }
+
+    //Nicole's attempt at making the Term Info box look a little better   
+    //I'm puting all the information into an html table.  rhs are each single table cells
+    //but are separated by line breaks.  
+  public static String termInfo(OBOClass oboClass) {
+    if (oboClass == null) {
+      System.out.println("null obo class for HtmlUtil.termInfo");
+      return ""; // null? exception?
+    }
+    StringBuffer sb = new StringBuffer();
+    sb.append("<table>");
+    if (oboClass.isObsolete()) {
+	sb.append(makeRow(makeLeftCol(bold("TERM"))+makeRightCol(bold(oboClass.getName())+colorFont(bold("   (OBSOLETE)"), "red"))));
+    }
+    else {
+	sb.append(makeRow(makeLeftCol(bold("TERM"))+makeRightCol(bold(oboClass.getName()))));
+    }
+    sb.append(makeRow(makeLeftCol(bold("ID"))+makeRightCol(oboClass.getID())));
+    
+    Set syns = oboClass.getSynonyms();
+    int pos = 0;
+    StringBuffer temp = new StringBuffer();
+    for (Iterator it = syns.iterator(); it.hasNext(); ) {
+	temp.append(it.next()+"<br>");
+	pos++;
+    }
+    if (pos>0) {
+	sb.append(makeRow(makeLeftCol(bold("Synonyms"))+makeRightCol(temp.toString())));
+    }
+
+    String definition = oboClass.getDefinition();
+
+    if ((definition != null) && !(definition.equals(""))) {
+	sb.append(makeRow(makeLeftCol(bold("Definition"))+makeRightCol(definition)));
+    }
+
+    sb.append(makeRow(makeLeftCol("")+makeRightCol("")));
+    sb.append(makeRow(makeLeftCol(bold("Parents"))+makeRightCol("<hr>")));
+    sb.append(getParentalString(oboClass));
+    sb.append(makeRow(""));
+    sb.append(makeRow(makeLeftCol(bold("Children"))+makeRightCol("<hr>")));
+    sb.append(getChildrenString(oboClass));
+    sb.append("</table>");
     return sb.toString();
   }
 
@@ -81,6 +130,34 @@ public class HtmlUtil {
     return "<b>"+text+"</b>";
   }
 
+  private static String italic(String text) {
+    if (!DO_HTML) return text;
+    return "<i>"+text+"</i>";
+  }
+
+    private static String colorFont(String text, String color) {
+	if (!DO_HTML) return text;
+	return "<font color="+color+">"+text+"</font>";
+    }
+
+    private static String makeRow(String text) {
+	if (!DO_HTML) return text;
+	return "<tr>"+text+"</tr>";
+    }
+    private static String makeLeftCol(String text) {
+	if (!DO_HTML) return text;
+	return "<td width=70 align=right valign=top><font size=-1>"+text+"</font></td>";
+    }
+    private static String makeRightCol(String text) {
+	if (!DO_HTML) return text;
+	return "<td align=left valign=top>"+text+"</td>";
+    }
+
+    private static String makeTable(String text) {
+	if (!DO_HTML) return text;
+	return "<table>"+text+"</table>";
+    }
+
   private static String nl() { return newLine(); }
 
   private static String newLine() {
@@ -101,53 +178,72 @@ public class HtmlUtil {
     StringBuffer sb = new StringBuffer();
     // or should thi sjust be done more generically with a hash of string bufs
     // for each unique link type name?
+    // YES...that would be a better method i think -nlw
+    //method:  make all the rt hand columns, then wrap around the left col of the type for the table entry
     StringBuffer isaStringBuf = new StringBuffer();
     StringBuffer partofStringBuf = new StringBuffer();
+    StringBuffer haspartStringBuf = new StringBuffer();
     StringBuffer devFromStringBuf = new StringBuffer();
     StringBuffer otherStringBuf = new StringBuffer();
+    int countIsa=0;
+    int countPartof=0;
+    int countHaspart=0;
+    int countDevfrom=0;
+    int countOther=0;
+   //i would like to clean this up to be able to group all relationships together, despite
+    //the fact that they could be non-standard, like in SO.  might need two for-loops.  
+    //would this be a drain on space/time?
     for (Iterator it = links.iterator(); it.hasNext(); ) {
       Link link = (Link)it.next();
       OBOProperty type = link.getType();
-      //sb.append(newLine());
-      //if (type == OBOProperty.IS_A) - somehow theres 2 instances???
       if (type.getName().equals("is_a")) {
-	isaStringBuf.append(newLine());
-        isaStringBuf.append(bold( isChild ? "Subclass" : "Superclass"));
-        isaStringBuf.append(bold("(ISA): "));
-	appendLink(isaStringBuf,isChild,link);
+	  countIsa++;
+	  appendLink(isaStringBuf,isChild,link);
       }
-      else if (type.getName().equals("part of")) {
-	partofStringBuf.append(newLine());
-        partofStringBuf.append(bold( isChild ? "Subpart: " : "Part of: "));
-	appendLink(partofStringBuf,isChild,link);
+      else if (type.getName().equals("part of") || type.getName().equals("part_of")) {	  
+	  countPartof++;
+	  appendLink(partofStringBuf,isChild,link);
       }
-      else if (type.getName().equals("develops from")) {
-	devFromStringBuf.append(newLine());
-	devFromStringBuf.append(bold( isChild ? "Develops into: ":"Develops from: "));
-	appendLink(devFromStringBuf,isChild,link);
+      else if (type.getName().equals("has part") || type.getName().equals("has_part")) {
+	  //i'm using this to catch the wierd reciprocal stuff in FMA
+	  //not sure yet if this is the best solution
+	  //probably need a new way to visualize reciprocal 'part' relationships
+	  countHaspart++;
+	  appendLink(haspartStringBuf,isChild,link);
+      }	  
+	else if (type.getName().equals("develops from") || type.getName().equals("develops_from")) {
+	    countDevfrom++;
+	    appendLink(devFromStringBuf,isChild,link);
+	}
+      else { //catch all other relationships
+	  countOther++;
+	  otherStringBuf.append("<tr>");
+	  otherStringBuf.append(makeLeftCol(italic(capitalize(type.getName())))+"<td>");
+	  appendLink(otherStringBuf,isChild,link);
+	  otherStringBuf.append("</td></tr>");
       }
-      // catch all - any relationships missed just do its name capitalize? _->' '?
-      else {
-	otherStringBuf.append(newLine());
-        otherStringBuf.append(bold(capitalize(type.getName()))).append(": ");
-	appendLink(otherStringBuf,isChild,link);
-      }
-//       if (isChild)
-//         termBuf.append(termLink(link.getChild()));
-//       else
-//         termBuf.append(termLink(link.getParent())); 
     }
-    sb.append(isaStringBuf).append(partofStringBuf);
-    sb.append(devFromStringBuf).append(otherStringBuf);
+    if (countIsa>0) 
+	sb.append(makeRow(makeLeftCol(italic( isChild ? "Subclass (is_a)" : "Superclass (is_a)"))+makeRightCol(isaStringBuf.toString())));
+    if (countPartof>0)
+	sb.append(makeRow(makeLeftCol(italic( isChild ? "Has part" : "Part of"))+makeRightCol(partofStringBuf.toString())));
+    if (countDevfrom>0)
+	sb.append(makeRow(makeLeftCol(italic( isChild ? "Develops into" : "Develops from"))+makeRightCol(devFromStringBuf.toString())));
+    if (countOther>0)
+	sb.append(makeRow(makeLeftCol(otherStringBuf.toString())));
     return sb;
   }
 
   private static void appendLink(StringBuffer sb, boolean isChild, Link link) {
-    if (isChild)
-      sb.append(termLink(link.getChild()));
-    else
-      sb.append(termLink(link.getParent())); 
-  }
+      if (isChild)
+	  sb.append(termLink(link.getChild())+"<br>");
+      else
+	  sb.append(termLink(link.getParent())+"<br>");
+      //    if (isChild)
+      //sb.append(makeRightCol(termLink(link.getChild())));
+      //else
+      //sb.append(makeRightCol(termLink(link.getParent()))); 
+      }
 
   private static String termLink(LinkedObject term) {
     String clickString = getClickString(term.getID(),term.getName());
