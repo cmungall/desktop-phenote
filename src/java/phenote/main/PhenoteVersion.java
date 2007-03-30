673c2510e417620b456993e2768a777cb31554cf
cmpich <cmpich@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2007-03-30 21:25:44 +0000

    added date for version release. Needs to be updated manually as you create a new version number.

--- src/java/phenote/main/PhenoteVersion.java
+++ src/java/phenote/main/PhenoteVersion.java
@@ -1,19 +1,27 @@
 package phenote.main;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /** Simple class for phenote version number - for standalone & servlet */
 
 public class PhenoteVersion {
-  
+
   //private static final float MAJOR_VERSION_NUM = 0.8f;
   //private static final float SUB_VERSION_NUM = .3f;
   private static final String VERSION = "1.2-beta1"; // ??
   // type is "dev" or "release" 
   //private static final String type = " dev"; // "release"
-  
+
   public static String versionString() {
     //return MAJOR_VERSION_NUM +""+ SUB_VERSION_NUM + type; 
     return VERSION;
   }
 
-  
+  public static Date getDateOfVersion(){
+    GregorianCalendar cal = new GregorianCalendar(2007, GregorianCalendar.MARCH, 16);
+    return cal.getTime();
+  }
+
+
 }
