dcdd1eff5bf045a7b2f6b764b7c2548856790fcc
mgibson <mgibson@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2007-08-30 17:39:25 +0000

    javadocs for group adapter i

--- src/java/phenote/dataadapter/GroupAdapterI.java
+++ src/java/phenote/dataadapter/GroupAdapterI.java
@@ -4,10 +4,19 @@ import phenote.edit.CharChangeListener;
 
 public interface GroupAdapterI {
 
+  /** If a group reacts to char changes(edits) then a char change listener should be 
+      added, for instance a user edits group fields and dest field is automatically 
+      repopulated, otherwise no-op */
   boolean hasCharChangeListener();
   CharChangeListener getCharChangeListener();
+  
+  /** similar to char change, for doing something on loading data into group, like
+      repopulating dest field, no-op if dont need this */
   boolean hasCharListChangeListener();
   CharListChangeListener getCharListChangeListener();
-  // boolean makesFieldValues()? makesRows? makesFieldNames?
+
+  /** the destination field that this group is populating (with obo classes), if
+      a group is not populating a destination field this would be no-oped
+      one could imagine other destinations like main datamodel */
   void setDestinationField(String field); // CharField? Ex?
 }
