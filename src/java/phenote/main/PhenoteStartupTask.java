7e81a18086d166ecc550e7d251ec1d69f26872b4
mgibson <mgibson@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2007-10-31 13:44:40 +0000

    added phenote startup task to be used in oboedit application wooshy which requires new oboedit jar which is very different from oboedit jar
    so eclipse people if you have dropped oboedit jar put it back in profile
    or just leave it in
    org.geneontology.jar still has to go though (replaced by bbop & obo jars)

---
+++ src/java/phenote/main/PhenoteStartupTask.java
@@ -0,0 +1,93 @@
+package phenote.main;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+
+import org.bbop.framework.GUIComponent;
+import org.bbop.framework.GUIComponentFactory;
+import org.bbop.framework.GUIComponentWrapper;
+import org.bbop.framework.ViewMenu;
+import org.bbop.util.CollectionUtil;
+import org.oboedit.gui.tasks.DefaultGUIStartupTask;
+
+public class PhenoteStartupTask extends DefaultGUIStartupTask {
+	@Override
+	protected Collection<GUIComponentFactory<?>> getDefaultComponentFactories() {
+		Collection<GUIComponentFactory<?>> factories = super.getDefaultComponentFactories();
+		factories.add(new PhenoteCompFactory());
+		return factories;
+	}
+	
+	@Override
+	protected String getAppID() {
+		return "phenote";
+	}
+	
+	@Override
+	protected String getAppName() {
+		return "Phenote";
+	}
+
+	@Override
+  // ??
+	protected String getPerspectiveResourceDir() { // not using org
+		return "phenote/gui/layout/resource";//"org/phenote/gui/layout/resources";
+	}
+
+	@Override
+  /** im not sure about this and svn & webstart? */
+	protected File getPrefsDir() {
+		return new File(System.getProperty("user.home") + "/.phenote");
+	}
+
+	@Override
+	protected Collection<? extends JMenuItem> getDefaultMenus() {
+		Collection<JMenuItem> menus = new ArrayList<JMenuItem>();
+		// add any other Phenote menus
+		menus.add(new ViewMenu());
+		return menus;
+	}
+
+  private class PhenoteCompFactory implements GUIComponentFactory {
+    public GUIComponent createComponent(String id) {
+      String l = "<html><h1>This is an example component</h1></html>";
+      return new GUIComponentWrapper(id, id, new JLabel(l));
+    }
+
+    public FactoryCategory getCategory() {
+      return FactoryCategory.MISC;
+    }
+
+    public String getDefaultID() {
+      return "main";
+    }
+
+    public List getIDs() {
+      return CollectionUtil.list("example_component");
+    }
+
+    public String getName() {
+      return "Example Component";
+    }
+
+    public boolean getPreferSeparateWindow() {
+      return false;
+    }
+
+    public boolean isSingleton() {
+      return false;
+    }
+
+    public boolean showInMenus() {
+      return true;
+    }
+
+
+  }
+
+}
