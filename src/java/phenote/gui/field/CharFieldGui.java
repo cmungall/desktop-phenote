eb7234a3ba5391daf3cd0ec50f9d291af610fd7b
mgibson <mgibson@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2007-11-27 20:08:48 +0000

    took out debug statements in char field gui

--- src/java/phenote/gui/field/CharFieldGui.java
+++ src/java/phenote/gui/field/CharFieldGui.java
@@ -671,10 +671,10 @@ public abstract class CharFieldGui implements ListSelectionListener {
   }
 
   private void updateListGui() {
-    log().debug("update list gui called has list"+hasListGui());
+    //log().debug("update list gui called has list"+hasListGui());
     if (!hasListGui()) return;
     List<CharacterI> l = getSelectedChars();
-    log().debug("update list gui called sel list size"+l.size());
+    //log().debug("update list gui called sel list size"+l.size());
     if (l==null || l.size()==0) {
       valueListModel.clear();
     }
