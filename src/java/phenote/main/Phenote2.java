241b3e02c96c4743159d1f7e0c3df814b553cd42
nomi <nomi@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2007-12-05 00:05:23 +0000

    Set application name on Mac so it doesn't use the ugly Java class name. (Shouldn't have any effect on non-Macs.)

--- src/java/phenote/main/Phenote2.java
+++ src/java/phenote/main/Phenote2.java
@@ -8,11 +8,13 @@ public class Phenote2 {
   
   private static String[] args;
 
-	public static void main(String[] args) {
-    Phenote2.args = args;
-		Runnable r = new PhenoteRunnable();
-		SwingUtilities.invokeLater(r);
-	}
+    public static void main(String[] args) {
+	Phenote2.args = args;
+	Runnable r = new PhenoteRunnable();
+	// Set application name on Mac so it doesn't use the ugly Java class name
+	System.setProperty("com.apple.mrj.application.apple.menu.about.name", getAppName());
+	SwingUtilities.invokeLater(r);
+    }
 
   private static class PhenoteRunnable implements Runnable {
     public void run() {
@@ -20,4 +22,8 @@ public class Phenote2 {
       GUIManager.getManager().start();
     }
   }
+
+    public static String getAppName() {
+	return "Phenote 2";
+    }
 }
