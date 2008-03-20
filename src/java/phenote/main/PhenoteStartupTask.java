f005b36a709afc247eee4bdb60fd34dba534ca2e
Jim Balhoff <balhoff@users.noreply.github.com>
2008-03-20 18:43:54 +0000

    New interface theme.

--- src/java/phenote/main/PhenoteStartupTask.java
+++ src/java/phenote/main/PhenoteStartupTask.java
@@ -10,12 +10,15 @@ import javax.swing.JMenuItem;
 import javax.swing.JToolBar;
 
 import org.apache.log4j.Logger;
+import org.bbop.framework.ComponentManager;
 import org.bbop.framework.GUIComponent;
 import org.bbop.framework.GUIComponentFactory;
 import org.bbop.framework.GUIManager;
 import org.bbop.framework.GUITask;
 import org.bbop.framework.ScreenLockTask;
 import org.bbop.framework.ViewMenu;
+import org.bbop.framework.dock.LayoutDriver;
+import org.bbop.framework.dock.idw.IDWDriver;
 import org.bbop.util.CollectionUtil;
 import org.oboedit.example.OBDAnnotationNumberFetchBehaviorTask;
 import org.oboedit.gui.Preferences;
@@ -28,14 +31,15 @@ import phenote.config.Config;
 import phenote.config.xml.GroupDocument.Group;
 import phenote.config.xml.TemplatechooserDocument.Templatechooser;
 import phenote.edit.DirtyDocumentIndicator;
+import phenote.gui.PhenoteDockingTheme;
 import phenote.gui.PhenoteMainFrame;
 import phenote.gui.StandardToolbar;
 import phenote.gui.actions.AboutAction;
 import phenote.gui.factories.CharacterTableFactory;
 import phenote.gui.factories.NCBIInfoFactory;
-import phenote.gui.factories.PhenoteTreeViewFactory;
 import phenote.gui.factories.PhenoteEditorFactory;
 import phenote.gui.factories.PhenoteGraphViewFactory;
+import phenote.gui.factories.PhenoteTreeViewFactory;
 import phenote.gui.factories.ProtocolEditorFactory;
 import phenote.gui.factories.TermInfoFactory;
 import phenote.gui.menu.EditMenu;
@@ -46,143 +50,158 @@ import phenote.gui.selection.SelectionBridge;
 
 public class PhenoteStartupTask extends DefaultGUIStartupTask {
 
-	private Logger LOG = Logger.getLogger(PhenoteStartupTask.class);
-	private String[] args;
-	private SelectionBridge selectionBridge = new SelectionBridge();
+  private Logger LOG = Logger.getLogger(PhenoteStartupTask.class);
+  private String[] args;
+  private SelectionBridge selectionBridge = new SelectionBridge();
 
-	PhenoteStartupTask(String[] args) {
-		this.args = args;
-		initPhenote();
-	}
+  PhenoteStartupTask(String[] args) {
+    this.args = args;
+    initPhenote();
+  }
 
-	@Override
-	protected Collection<GUIComponentFactory<?>> getDefaultComponentFactories() {
-		// Collection<GUIComponentFactory<?>> factories =
-		// super.getDefaultComponentFactories();
-		Collection<GUIComponentFactory<?>> factories = new ArrayList<GUIComponentFactory<?>>();
-//		factories.add(new FieldPanelFactory());
-		factories.add(new PhenoteEditorFactory());
-		factories.add(new TermInfoFactory());
-		factories.add(new NCBIInfoFactory());
-		factories.addAll(this.getCharacterTableComponentFactories());
-//		factories.add(new GraphEditorFactory());  <-- removing this for upcoming 1.5 release
-		factories.add(new PhenoteTreeViewFactory());
-		factories.add(new PhenoteGraphViewFactory());
-		factories.add(new AnnotationSummaryComponentFactory());
-		factories.addAll(this.getTemplateGroupComponentFactories());
+  @Override
+  protected Collection<GUIComponentFactory<?>> getDefaultComponentFactories() {
+    // Collection<GUIComponentFactory<?>> factories =
+    // super.getDefaultComponentFactories();
+    Collection<GUIComponentFactory<?>> factories = new ArrayList<GUIComponentFactory<?>>();
+//  factories.add(new FieldPanelFactory());
+    factories.add(new PhenoteEditorFactory());
+    factories.add(new TermInfoFactory());
+    factories.add(new NCBIInfoFactory());
+    factories.addAll(this.getCharacterTableComponentFactories());
+//  factories.add(new GraphEditorFactory());  <-- removing this for upcoming 1.5 release
+    factories.add(new PhenoteTreeViewFactory());
+    factories.add(new PhenoteGraphViewFactory());
+    factories.add(new AnnotationSummaryComponentFactory());
+    factories.addAll(this.getTemplateGroupComponentFactories());
     factories.add(new ProtocolEditorFactory());
-		return factories;
-	}
+    return factories;
+  }
 
-	/** this is called at initialization i believe */
-	@Override
-	protected void doOtherInstallations() {
-	}
-	
-	/** perhaps this might change in the future so both oboedit and phenote will
-	 *  use the same default toolbars with save, exit, etc. buttons
-	 */
-	@Override
-	protected Collection<JToolBar> getDefaultToolBars() {
-		LOG.debug("getting Default Toolbars");
-		Collection<JToolBar> toolbars = new ArrayList<JToolBar>();
-		GUIComponent tb = new StandardToolbar();
-		toolbars.add((JToolBar) tb.getComponent());
-		return toolbars;
-		// new Throwable().printStackTrace();
-	}
-	
+  /** this is called at initialization i believe */
+  @Override
+  protected void doOtherInstallations() {
+  }
 
-	private void initPhenote() {
-		Phenote.initBackend(args);
-		selectionBridge.install();
-		// init backend sets off splash screent that must be destroyed
-		Phenote.getPhenote().splashScreenDestruct();
-	}
+  /** perhaps this might change in the future so both oboedit and phenote will
+   *  use the same default toolbars with save, exit, etc. buttons
+   */
+  @Override
+  protected Collection<JToolBar> getDefaultToolBars() {
+    LOG.debug("getting Default Toolbars");
+    Collection<JToolBar> toolbars = new ArrayList<JToolBar>();
+    GUIComponent tb = new StandardToolbar();
+    toolbars.add((JToolBar) tb.getComponent());
+    return toolbars;
+    // new Throwable().printStackTrace();
+  }
 
-	@Override
-	protected String getAppID() {
-		return "phenote";
-	}
 
-	@Override
-	protected String getAppName() {
-	  return Phenote2.getAppName();
-	}
+  private void initPhenote() {
+    Phenote.initBackend(args);
+    selectionBridge.install();
+    // init backend sets off splash screent that must be destroyed
+    Phenote.getPhenote().splashScreenDestruct();
+  }
 
-	protected Action getAboutAction() {
-		return new AboutAction();
-	}
+  @Override
+  protected String getAppID() {
+    return "phenote";
+  }
 
-	/** in this directory obo/idw expects to find a file named perspectives.xml
+  @Override
+  protected String getAppName() {
+    return Phenote2.getAppName();
+  }
+
+  protected Action getAboutAction() {
+    return new AboutAction();
+  }
+
+  /** in this directory obo/idw expects to find a file named perspectives.xml
       which then points to idw files in this same directory 
       obo can get this from a jar but doesnt work from webstart jar???
       FileNotFoundException printed to stdout if not found and no perspectives
       come up */
-	@Override
-	protected String getPerspectiveResourceDir() { // not using org
-		return "phenote/gui/layout/resources"; // need to add this
-		// "org/phenote/gui/layout/resources";
-		// return System.getProperty("user.home") + "/.phenote";
-	}
+  @Override
+  protected String getPerspectiveResourceDir() { // not using org
+    return "phenote/gui/layout/resources"; // need to add this
+    // "org/phenote/gui/layout/resources";
+    // return System.getProperty("user.home") + "/.phenote";
+  }
 
-	@Override
-	protected String getDefaultPerspectiveResourcePath() {
-		if (getPerspectiveResourceDir() != null)
-			return getPerspectiveResourceDir() + "/phenote_classic.idw";
-		else
-			return null;
-	}
+  @Override
+  protected String getDefaultPerspectiveResourcePath() {
+    if (getPerspectiveResourceDir() != null)
+      return getPerspectiveResourceDir() + "/phenote_classic.idw";
+    else
+      return null;
+  }
 
-	@Override
-	/** im not sure about this and svn & webstart?
+  @Override
+  /** im not sure about this and svn & webstart?
    so idw/obo adds a perspective subdir here and copies perspectives here
    from jars/classes */
-	protected File getPrefsDir() {
-		// .phenote/layout?
-		return new File(System.getProperty("user.home") + "/.phenote");
-	}
+  protected File getPrefsDir() {
+    // .phenote/layout?
+    return new File(System.getProperty("user.home") + "/.phenote");
+  }
 
-	@Override
-	protected Collection<? extends JMenuItem> getDefaultMenus() {
-		Collection<JMenuItem> menus = new ArrayList<JMenuItem>();
-		// add any other Phenote menus
-		menus.add(new FileMenu());
-		menus.add(new EditMenu());
-		menus.add(new ViewMenu());
-		menus.add(new SettingsMenu());
-		menus.add(new PhenoteHelpMenu());
-		return menus;
-	}
+  @Override
+  protected Collection<? extends JMenuItem> getDefaultMenus() {
+    Collection<JMenuItem> menus = new ArrayList<JMenuItem>();
+    // add any other Phenote menus
+    menus.add(new FileMenu());
+    menus.add(new EditMenu());
+    menus.add(new ViewMenu());
+    menus.add(new SettingsMenu());
+    menus.add(new PhenoteHelpMenu());
+    return menus;
+  }
 
-	@Override
-	protected Collection<GUITask> getDefaultTasks() {
-		ScreenLockTask screenLockTask = new ScreenLockTask(GUIManager
-				.getManager().getScreenLockQueue(), GUIManager.getManager()
-				.getFrame(), Preferences.getPreferences()
-				.getUseModalProgressMonitors());
-		return CollectionUtil.list((GUITask) screenLockTask,
-				new OBDAnnotationNumberFetchBehaviorTask()); // TODO: for
-		// testing
-	}
-	
-	@Override
+  @Override
+  protected Collection<GUITask> getDefaultTasks() {
+    ScreenLockTask screenLockTask = new ScreenLockTask(GUIManager
+        .getManager().getScreenLockQueue(), GUIManager.getManager()
+        .getFrame(), Preferences.getPreferences()
+        .getUseModalProgressMonitors());
+    return CollectionUtil.list((GUITask) screenLockTask,
+        new OBDAnnotationNumberFetchBehaviorTask()); // TODO: for
+    // testing
+  }
+
+  @Override
   protected void installSystemListeners() {
-	  GUIManager.addVetoableShutdownListener(new DirtyDocumentIndicator());
-	}
-	
-	@Override
-	protected JFrame createFrame() {
-		JFrame out = new PhenoteMainFrame(getAppID());
-		out.setTitle(getAppName());
-		return out;
-	}
-	
-	/**
-	 * Create a character table factory configured for each group in config
-	 */
-	private Collection<CharacterTableFactory> getCharacterTableComponentFactories() {
-	  Collection<CharacterTableFactory> factories = new ArrayList<CharacterTableFactory>();
+    GUIManager.addVetoableShutdownListener(new DirtyDocumentIndicator());
+  }
+
+  @Override
+  protected JFrame createFrame() {
+    JFrame out = new PhenoteMainFrame(getAppID());
+    out.setTitle(getAppName());
+    return out;
+  }
+
+  @Override
+  protected void configureUI() {
+    // overriding the odd colors in BBOP framework
+  }
+
+  @Override
+  public void run() {
+    super.run();
+    // driver needs to be set after all of BBOP framework is initialized
+    final LayoutDriver driver = ComponentManager.getManager().getDriver();
+    if (driver instanceof IDWDriver) {
+      ((IDWDriver)driver).setTheme(new PhenoteDockingTheme());
+    }
+  }
+
+  /**
+   * Create a character table factory configured for each group in config
+   */
+  private Collection<CharacterTableFactory> getCharacterTableComponentFactories() {
+    Collection<CharacterTableFactory> factories = new ArrayList<CharacterTableFactory>();
     for (Group group : Config.inst().getFieldGroups()) {
       log().debug("Creating table factory for: " + group.getName());
       if (!group.getInterface().equals(Group.Interface.CHARACTER_TEMPLATE)) {
@@ -194,9 +213,9 @@ public class PhenoteStartupTask extends DefaultGUIStartupTask {
       factories.add(new CharacterTableFactory(this.getDefaultGroup()));
     }
     return factories;
-	}
-	
-	private Collection<GUIComponentFactory<?>> getTemplateGroupComponentFactories() {
+  }
+
+  private Collection<GUIComponentFactory<?>> getTemplateGroupComponentFactories() {
     Collection<GUIComponentFactory<?>> factories = new ArrayList<GUIComponentFactory<?>>();
     for (Group group : Config.inst().getFieldGroups()) {
       if (group.getInterface().equals(Group.Interface.CHARACTER_TEMPLATE)) {
@@ -209,11 +228,11 @@ public class PhenoteStartupTask extends DefaultGUIStartupTask {
     return factories;
   }
 
-	private String getDefaultGroup() {
-		return Config.inst().getDefaultGroup().getName();
-	}
-	
-	private Logger log() {
+  private String getDefaultGroup() {
+    return Config.inst().getDefaultGroup().getName();
+  }
+
+  private Logger log() {
     return Logger.getLogger(this.getClass());
   }
 }
