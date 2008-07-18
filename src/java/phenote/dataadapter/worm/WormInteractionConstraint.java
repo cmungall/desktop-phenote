482a48cfb95475f4b65f465f3ae297ee5dec7114
azurebrd <azurebrd@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2008-07-18 23:28:49 +0000

    Added constraint to worm interaction (data currently in the database doesn't meet constraints)

---
+++ src/java/phenote/dataadapter/worm/WormInteractionConstraint.java
@@ -0,0 +1,140 @@
+package phenote.dataadapter.worm;
+
+import java.util.List;
+
+import phenote.datamodel.CharacterI;
+import phenote.edit.EditManager;
+
+
+import phenote.dataadapter.AbstractCommitConstraint;
+import phenote.dataadapter.ConstraintStatus;
+import phenote.dataadapter.Status;
+
+
+public class WormInteractionConstraint extends AbstractCommitConstraint {
+
+  
+  @Override
+  /** Overrides AbsCC to check deletes */
+  public ConstraintStatus checkCommit() {
+    boolean warning = false;
+    String message = "";
+
+    // Check for DELETES
+    List<CharacterI> l = EditManager.inst().getDeletedAnnotations();
+    try {
+      for (CharacterI echr : l) {
+        String joinkey = echr.getValueString("PgdbId");
+        if (joinkey.equals("")) { continue; }
+        message += "Deleting character for "+joinkey+".  ";  warning = true;	// give warning for any deleted characters
+      }
+    } catch (Exception e) {
+      System.out.println("Could not delete character: " + e);
+    }
+    ConstraintStatus statusBundle = ConstraintStatus.makeOK();
+    if (warning) 
+      statusBundle.addStatusChild(new ConstraintStatus(Status.WARNING,message));
+   
+    // CHECK EACH CHAR
+    // from abstract - calls checkCharCommit for each char
+    ConstraintStatus charListStatus = checkEachChar();
+    statusBundle.addStatusChild(charListStatus);
+
+    return statusBundle;
+  }
+
+  /** do constraint check for commit time on a character
+   return ConstraintStatus indication if constraint passed and error msg */
+  protected ConstraintStatus checkCharCommit(CharacterI chr) {
+    
+    boolean warning = false;
+
+    String message = ""; // or StringBuffer?
+    String temp_message = "";
+
+    Integer flagBad = 0;
+
+
+    try {
+      String pgdbid = chr.getValueString("PgdbId");
+      if ( (pgdbid == null) || (pgdbid == "") ) { 
+        String intID = chr.getValueString("Interaction ID");
+        message += "New character for "+intID+".  ";  warning = true;	// give warning for any new characters
+        pgdbid = "with new value for "+intID;
+      }
+
+//      String tag_name = "Allele Status";
+//      if ( chr.hasValue(tag_name) ) { 
+//        String allele_status = chr.getTerm(tag_name).getID(); 
+//        if (allele_status.equals("mapped_wrong")) { 
+//          if (chr.hasValue("Object Remark")) { } else {
+//            warning = true; message += "Character "+pgdbid+" Mapped Wrong and has no Object Remark.  "; } } }
+
+      if (chr.hasValue("Curator")) { } else { 
+        flagBad++;
+        message += "Character "+pgdbid+" has no Curator.  "; }
+
+      if ( (chr.hasValue("Person")) || (chr.hasValue("Pub")) ) { } else { 
+        flagBad++;
+        message += "Character "+pgdbid+" has neither Person nor Paper.  "; }
+  
+      if ( (chr.hasValue("Effector")) || (chr.hasValue("tor Variation")) || (chr.hasValue("tor Transgene")) ) { } else { 
+        flagBad++;
+        message += "Character "+pgdbid+" has neither Effector nor tor Variation nor tor Transgene.  "; }
+
+      if (chr.hasValue("Type")) { } else { 
+        flagBad++;
+        message += "Character "+pgdbid+" has no Type.  "; }
+
+      if ( (chr.hasValue("Effected")) || (chr.hasValue("ted Variation")) || (chr.hasValue("ted Transgene")) ) { } else { 
+        flagBad++;
+        message += "Character "+pgdbid+" has neither Effected nor ted Variation nor ted Transgene.  "; }
+
+      if (chr.hasValue("Phenotype")) { } else { 
+        flagBad++;
+        message += "Character "+pgdbid+" has no Phenotype Term.  "; }
+
+
+//      if (chr.hasValue("NBP")) { 
+//          return ConstraintStatus.makeOK(); }
+//        else { 
+//          temp_message += "Character "+pgdbid+" has no NBP.  "; }	// only add this if flagBad is bad (no NBP is okay if other fields are okay)
+  
+  
+  
+      if (flagBad > 0) { warning = true; message += temp_message; }
+
+
+//      if (chr.hasValue("NBP")) { } else { 
+//        warning = true;
+//        message += "Character has no NBP for char: "+chr; }
+//         if (chr.hasValue("Object Name")) { } else { 
+//           warning = true;
+//           message += "Character has no Object Name"; }
+//      
+    // get fields ya need from chr
+    // chr.hasValue("NBP")
+      // ...
+    
+    
+      // if doesnt have what you need... make a warning or failure?
+      // if yada is null || xyz is null...
+      // warning = true;
+      // message += "Character such&such is lacking such&such";
+
+    
+    } catch (Exception e) {
+      System.out.println("Could not delete character: " + e);
+    }
+
+    if (warning) 
+      return new ConstraintStatus(Status.WARNING,message);
+    
+    else
+      return ConstraintStatus.makeOK();
+
+
+  }
+
+
+}
