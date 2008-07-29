8e7fcb517504d6d694c2bc3418d52cd9e7962700
mwallinga <mwallinga@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2008-07-29 16:59:19 +0000

    This class is just a placeholder for now.

--- src/java/phenote/matrix/model/MatrixController.java
+++ src/java/phenote/matrix/model/MatrixController.java
@@ -4,32 +4,5 @@ import phenote.datamodel.CharFieldException;
 import ca.odell.glazedlists.BasicEventList;
 
 public class MatrixController {
-	MatrixBuilder mb;
 	
-	/**
-	 *  A new MatrixController creates a new MatrixBuilder with the specified grouping field string.
-	 *  This should be refactored to be more general (use a String parameter of something instead of 
-	 *  hard-coding Taxa).
-	 */
-	public MatrixController () {
-		mb = new MatrixBuilder("Valid Taxon");
-	}
-	
-	/**
-	 * A method to access the matrix associated with this controller's MatrixBuilder
-	 * 
-	 * @return The matrix from the associated MatrixBuilder
-	 */
-	public BasicEventList<MatrixRow> getMatrix() {
-		return mb.getMatrix();
-	}
-	
-	public void buildMatrixCharList() throws CharFieldException {
-	  mb.buildSortedList();
-	  mb.buildMatrix();
-	}
-	
-	public int getMatrixSize() {
-	  return mb.getNumRows();
-	}
 }
