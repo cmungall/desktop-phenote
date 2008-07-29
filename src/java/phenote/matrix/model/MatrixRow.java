2e84cb34313e7aac555311b5939d58d51d3917f7
mwallinga <mwallinga@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2008-07-29 16:57:03 +0000

    Added another interface to my data model.

--- src/java/phenote/matrix/model/MatrixRow.java
+++ src/java/phenote/matrix/model/MatrixRow.java
@@ -1,69 +1,19 @@
 package phenote.matrix.model;
 
-import ca.odell.glazedlists.BasicEventList;
-import org.obo.datamodel.OBOClass;
+import phenote.datamodel.CharacterI;
 
-public class MatrixRow {
-	private String rowFieldName;
-	private String rowValue;
-	private BasicEventList<MatrixRowElement> columns;
-	
-	public MatrixRow (String rfn, String rv) {
-		rowFieldName = rfn;
-		rowValue = rv;
-		columns = new BasicEventList<MatrixRowElement>();
-	}
-		
-	/**
-	 * A new column is added to a matrix row by creating a new name (heading) for the column using 
-	 * the entity and attribute term names, and the value is retrieved from the value term name.
-	 * 
-	 * @param entityTerm An OBOClass representing the entity
-	 * @param attributeTerm An OBOClass representing the attribute
-	 * @param valueTerm An OBOClass representing the value
-	 */
-	public void addColumn(OBOClass entityTerm, OBOClass attributeTerm, OBOClass valueTerm) {
-	  String name, name1, name2, value;
-	  name1 = entityTerm != null ? entityTerm.getName() : "";
-	  name2 = attributeTerm != null ? attributeTerm.getName() : "";
-	  name = name1 + " " + name2;
-		value = valueTerm.getName();
-		columns.add(new MatrixRowElement(name, value));
-	}
+public interface MatrixRow {
+  
+  /** 
+   * @param aCharacter - given a Character from the main data list
+   * @return true if the character has a value for this row
+   */
+  public boolean isValue (CharacterI aCharacter);
+  
+  /**
+   * @param aCharacter - given a Character from the main data list
+   * @return the Character's value for this particular row
+   */
+  public Object getValue (CharacterI aCharacter);
 
-	/**
-	 * Set the field name for this row.
-	 * 
-	 * @param rfn The new field name for this row in the matrix 
-	 */
-	public void setRowFieldName(String rfn) {
-		this.rowFieldName = rfn;
-	}
-
-	/**
-	 * Retrieve the row's field name.
-	 * 
-	 * @return The field name of the current row
-	 */
-	public String getRowFieldName() {
-		return rowFieldName;
-	}
-
-	/**
-	 * Set the value for the current row
-	 * 
-	 * @param rv The new value for the current row
-	 */
-	public void setRowValue(String rv) {
-		this.rowValue = rv;
-	}
-
-	/**
-	 * Retrieve the row's value
-	 * 
-	 * @return The value of the current row
-	 */
-	public String getRowValue() {
-		return rowValue;
-	}
 }
