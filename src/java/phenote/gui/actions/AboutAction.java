b3b47bbc05c86ac44374d0f9fb8cf166025f3da6
nomi <nomi@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2010-01-11 19:38:16 +0000

    Share the About code with PhenoteHelpMenu

--- src/java/phenote/gui/actions/AboutAction.java
+++ src/java/phenote/gui/actions/AboutAction.java
@@ -2,24 +2,22 @@ package phenote.gui.actions;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+//import java.awt.event.MouseEvent;
+//import java.awt.event.MouseListener;
 
 import javax.swing.AbstractAction;
-import javax.swing.ImageIcon;
+//import javax.swing.ImageIcon;
 
-import phenote.gui.SplashScreen;
-import phenote.main.PhenoteVersion;
+import phenote.gui.menu.PhenoteHelpMenu;
+//import phenote.main.PhenoteVersion;
 
-
-//This is the action for bringing up a splash screen to show the phenote
+//This is the action for bringing up a (small) splash screen to show the phenote
 //version info...possibly other stuff in the future.
 
 public class AboutAction extends AbstractAction {
 
-	public SplashScreen splashScreen;
-  private String logoFile = "src/java/phenote/images/phenote_logo.jpg";
-
+//	public SplashScreen splashScreen;
+//	private String logoFile = "src/java/phenote/images/phenote_logo.jpg";
 
 	public AboutAction() {
 		super("About Phenote");
@@ -29,36 +27,33 @@ public class AboutAction extends AbstractAction {
 		setEnabled(true);
 	}
 	public void actionPerformed(ActionEvent e) {
-		//log this action
-		splashScreenInit();
+//		splashScreenInit();
+		PhenoteHelpMenu.showAboutFrame();
 		
 //		System.out.println(e.getActionCommand().toString()+" action selected by:\n  "+ e);
 	}
 	
-  private void splashScreenInit() {
-  	ImageIcon myImage = new ImageIcon(logoFile);
-  	SplashScreenListener splashScreenListener = new SplashScreenListener();
-  	splashScreen = new SplashScreen(myImage,true); // true->enable
-  	splashScreen.setLocationRelativeTo(null);
-//	splashScreen.setProgressMax(100);
-  	splashScreen.setScreenVisible(true);
-  	splashScreen.addMouseListener(splashScreenListener);
-  	splashScreen.setProgress("Phenote version "+PhenoteVersion.versionString(), 0);
+//   public void splashScreenInit() {
+//   	ImageIcon myImage = new ImageIcon(logoFile);
+//   	SplashScreenListener splashScreenListener = new SplashScreenListener();
+//   	splashScreen = new SplashScreen(myImage,true); // true->enable
+//   	splashScreen.setLocationRelativeTo(null);
+// //	splashScreen.setProgressMax(100);
+//   	splashScreen.setScreenVisible(true);
+//   	splashScreen.addMouseListener(splashScreenListener);
+//   	splashScreen.setProgress("Phenote version "+PhenoteVersion.versionString(), 0);
+//   }
 
-  }
-
-  private class SplashScreenListener implements MouseListener {
-		public void mouseClicked(MouseEvent e) {
-		  //the splash screen will go away when clicked w/ mouse
-		  splashScreen.setScreenVisible(false);
-		  splashScreen = null;
-		  return;
-		}	
-		public void mouseExited(MouseEvent e) {;}
-	    public void mousePressed(MouseEvent e) {;}
-	    public void mouseReleased(MouseEvent e) {;}
-	    public void mouseEntered(MouseEvent e) {;}
-	  }	
+//   private class SplashScreenListener implements MouseListener {
+// 		public void mouseClicked(MouseEvent e) {
+// 		  //the splash screen will go away when clicked w/ mouse
+// 		  splashScreen.setScreenVisible(false);
+// 		  splashScreen = null;
+// 		  return;
+// 		}	
+// 		public void mouseExited(MouseEvent e) {;}
+// 	    public void mousePressed(MouseEvent e) {;}
+// 	    public void mouseReleased(MouseEvent e) {;}
+// 	    public void mouseEntered(MouseEvent e) {;}
+//	  }	
 }  
-
-
