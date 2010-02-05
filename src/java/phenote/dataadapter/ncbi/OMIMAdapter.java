697444e34a76ee8843f4060dfd8fdef4cd5cab4a
nomi <nomi@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2010-02-05 19:39:05 +0000

    Fixed incorrect call to getURLConnectionWithOrWithoutProxy. Also fixed indenting (the random indenting was hurting my eyes).

--- src/java/phenote/dataadapter/ncbi/OMIMAdapter.java
+++ src/java/phenote/dataadapter/ncbi/OMIMAdapter.java
@@ -33,18 +33,19 @@ import phenote.util.FileUtil;
  * @author Nicole Washington
  *
  */
+
 //public class OMIMAdapter implements QueryableDataAdapterI {
-	public class OMIMAdapter implements NCBIDataAdapterI {
+public class OMIMAdapter implements NCBIDataAdapterI {
 
 //	private static final String OMIMurlBase = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=omim";
-		private static final String database = "omim";
-	private static final String OMIMurlBase = ncbiURL+"db="+database;
-	private static final String returnMode = "&retmode=xml";
-	private static final String testURL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=omim&id=601653&retmode=xml";
-	//SNOMED access via id=http://terminology.vetmed.vt.edu/SCT/isa.cfm?SCT_ConceptID=190787008
-	//External links for GeneRIF: http://www.ncbi.nlm.nih.gov/sites/entrez?cmd_current=&db=gene&orig_db=genome&term=GeneRIF%3A12551903&cmd=Search
+  private static final String database = "omim";
+  private static final String OMIMurlBase = ncbiURL+"db="+database;
+  private static final String returnMode = "&retmode=xml";
+  private static final String testURL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=omim&id=601653&retmode=xml";
+  //SNOMED access via id=http://terminology.vetmed.vt.edu/SCT/isa.cfm?SCT_ConceptID=190787008
+  //External links for GeneRIF: http://www.ncbi.nlm.nih.gov/sites/entrez?cmd_current=&db=gene&orig_db=genome&term=GeneRIF%3A12551903&cmd=Search
 	
-	private List<String> queryableFields = new ArrayList<String>(2);
+  private List<String> queryableFields = new ArrayList<String>(2);
   private List<String> queryableGroups = new ArrayList<String>(2);
 
   public OMIMAdapter() { init(); }
@@ -55,8 +56,8 @@ import phenote.util.FileUtil;
 
   public String getOMIMbyID(String id) {
 //  public String getOMIMbyID(String id) throws DataAdapterEx {
-  	String omimURL = null;
-  	URL u = null;
+    String omimURL = null;
+    URL u = null;
     StringBuffer sb = new StringBuffer();
 //  	return "SOME OMIM STUFF HERE";
     String[] splitID=id.split(":");
@@ -65,29 +66,29 @@ import phenote.util.FileUtil;
     try {
       omimURL = OMIMurlBase+"&id="+idNum+returnMode;
       System.out.println(omimURL);
-    	u = new URL(omimURL); // throws MalformedURLEx
+      u = new URL(omimURL); // throws MalformedURLEx
     } catch (MalformedURLException e) {}
     try {
 //    URLConnection uc = u.openConnection();
-    URLConnection uc = getURLConnectionWithOrWithoutProxy(u);
-    BufferedReader in = new BufferedReader(new InputStreamReader(
-        uc.getInputStream()));
-    String inputLine;
-    while ((inputLine = in.readLine()) != null) {
+      URLConnection uc = FileUtil.getURLConnectionWithOrWithoutProxy(u);
+      BufferedReader in = new BufferedReader(new InputStreamReader(
+                                               uc.getInputStream()));
+      String inputLine;
+      while ((inputLine = in.readLine()) != null) {
     	System.out.println(inputLine);
     	sb.append(inputLine);
-    }
-    in.close();
+      }
+      in.close();
     } catch (IOException e) {} ;
     return sb.toString();
-   }
+  }
 
 
   private void init() {
     // dont HAVE to use CharFieldEnum but it does enforce using same strings
     // across different data adapters which is good to enforce
     // the worm config needs to have "Pub" and "Object Name"
-  	//might eventually add other ncbi options here
+    //might eventually add other ncbi options here
 //  	queryableFields.add(CharFieldEnum.PUB.getName()); // "Pub"
     queryableFields.add("OMIM");
   }
@@ -97,79 +98,79 @@ import phenote.util.FileUtil;
   }
 
   public String query(String id, String database) {
-  	String omimURL = null;
-  	URL u = null;
+    String omimURL = null;
+    URL u = null;
     StringBuffer sb = new StringBuffer();
     String[] splitID=id.split(":");
     String idSpace= splitID[0];
     String idNum = splitID[1];
     if (database.equalsIgnoreCase(idSpace)) {
-    	//make sure the idspace and database match
-    	System.out.println("same database!");
+      //make sure the idspace and database match
+      System.out.println("same database!");
     }
     try {
       omimURL = OMIMurlBase+"&id="+idNum+returnMode;
       System.out.println(omimURL);
-    	u = new URL(omimURL); // throws MalformedURLEx
+      u = new URL(omimURL); // throws MalformedURLEx
     } catch (MalformedURLException e) {}
     try {
-    URLConnection uc = u.openConnection();
-    BufferedReader in = new BufferedReader(new InputStreamReader(
-        uc.getInputStream()));
-    String inputLine;
-    while ((inputLine = in.readLine()) != null) {
+      URLConnection uc = u.openConnection();
+      BufferedReader in = new BufferedReader(new InputStreamReader(
+                                               uc.getInputStream()));
+      String inputLine;
+      while ((inputLine = in.readLine()) != null) {
     	System.out.println(inputLine);
     	sb.append(inputLine);
-    }
-    in.close();
+      }
+      in.close();
     } catch (IOException e) {} ;
     return sb.toString();  	
   }
   
   public Instance query(String id) {
-  	Instance pubInstance=null;
-		OBOSession session = CharFieldManager.inst().getOboSession();
+    Instance pubInstance=null;
+    OBOSession session = CharFieldManager.inst().getOboSession();
     String idSpace = null;
     String idNum = null;
     if (id!=null) {
-    	String[] splitID=id.split(":");
-    	if (splitID.length>1) {
-    		idSpace= splitID[0];
-    		idNum = splitID[1];
-    	}
+      String[] splitID=id.split(":");
+      if (splitID.length>1) {
+        idSpace= splitID[0];
+        idNum = splitID[1];
+      }
     }
     try
     {
-        EUtilsServiceLocator service = new EUtilsServiceLocator();
-        EUtilsServiceSoap utils = service.geteUtilsServiceSoap();
-        // call NCBI EFetch utility
-        EFetchRequest parameters = new EFetchRequest();
-        parameters.setDb(database);
-        parameters.setId(idNum);
-        EFetchResult res = utils.run_eFetch(parameters);
-        // results output
-        MimEntryType mimEntry = null;
-        if (res!=null && res.getMimEntries()!=null) {
-        	//just get the first one.
-        	for(int i=0; i<res.getMimEntries().getMimEntry().length; i++)
-        	{	
-        		mimEntry = res.getMimEntries().getMimEntry()[i];
-        		pubInstance = (Instance)session.getObjectFactory().createObject(id, AnnotationOntology.PUBLICATION(), false);        	
-        		if (mimEntry.getMimEntry_title()!=null)
-        			pubInstance.setName(mimEntry.getMimEntry_title());
+      EUtilsServiceLocator service = new EUtilsServiceLocator();
+      EUtilsServiceSoap utils = service.geteUtilsServiceSoap();
+      // call NCBI EFetch utility
+      EFetchRequest parameters = new EFetchRequest();
+      parameters.setDb(database);
+      parameters.setId(idNum);
+      EFetchResult res = utils.run_eFetch(parameters);
+      // results output
+      MimEntryType mimEntry = null;
+      if (res!=null && res.getMimEntries()!=null) {
+        //just get the first one.
+        for(int i=0; i<res.getMimEntries().getMimEntry().length; i++)
+        {	
+          mimEntry = res.getMimEntries().getMimEntry()[i];
+          pubInstance = (Instance)session.getObjectFactory().createObject(id, AnnotationOntology.PUBLICATION(), false);        	
+          if (mimEntry.getMimEntry_title()!=null)
+            pubInstance.setName(mimEntry.getMimEntry_title());
         		
-        		if (mimEntry.getMimEntry_synonyms()!=null) {
-        			for (int j=0; j<mimEntry.getMimEntry_synonyms().getMimEntry_synonyms_E().length; j++) {
+          if (mimEntry.getMimEntry_synonyms()!=null) {
+            for (int j=0; j<mimEntry.getMimEntry_synonyms().getMimEntry_synonyms_E().length; j++) {
 //        			pubInstance.addSynonym(mimEntry.getMimEntry_synonyms().getMimEntry_synonyms_E()[j]);
-        			}
-        		}
-        		if (mimEntry.getMimEntry_clinicalSynopsis()!=null)
-        			pubInstance.setDefinition(mimEntry.getMimEntry_clinicalSynopsis().toString());
-        		if (mimEntry.getMimEntry_summary()!=null)
-        			pubInstance.setDefinition(mimEntry.getMimEntry_summary().getMimText(0).getMimText_text());   
-        		pubInstance.setComment(makeCitationString(mimEntry));
-        	}
-        } else { pubInstance=null; }
+            }
+          }
+          if (mimEntry.getMimEntry_clinicalSynopsis()!=null)
+            pubInstance.setDefinition(mimEntry.getMimEntry_clinicalSynopsis().toString());
+          if (mimEntry.getMimEntry_summary()!=null)
+            pubInstance.setDefinition(mimEntry.getMimEntry_summary().getMimText(0).getMimText_text());   
+          pubInstance.setComment(makeCitationString(mimEntry));
+        }
+      } else { pubInstance=null; }
     }
     catch(Exception e) { System.out.println(e.toString()); pubInstance=null;}
     
@@ -184,58 +185,58 @@ import phenote.util.FileUtil;
   public String getName() { return database; }
   
   private String makeCitationString (MimEntryType mimEntry) {
-  	//this ought to e a util
-  	String citation = "";
-  	String synonyms="";
-  	String synopsis="";
-  	String summary="";
-  	String symbol=" ";
-  	String title="";
-  	String allelicVariants="";
-  	MimEntry_clinicalSynopsisType clinicalSynopsis = mimEntry.getMimEntry_clinicalSynopsis();
+    //this ought to e a util
+    String citation = "";
+    String synonyms="";
+    String synopsis="";
+    String summary="";
+    String symbol=" ";
+    String title="";
+    String allelicVariants="";
+    MimEntry_clinicalSynopsisType clinicalSynopsis = mimEntry.getMimEntry_clinicalSynopsis();
 
-  	if (mimEntry.getMimEntry_symbol()!=null)
-  		symbol = mimEntry.getMimEntry_symbol();
-  	if (mimEntry.getMimEntry_title()!=null)
-  		title = mimEntry.getMimEntry_title();
+    if (mimEntry.getMimEntry_symbol()!=null)
+      symbol = mimEntry.getMimEntry_symbol();
+    if (mimEntry.getMimEntry_title()!=null)
+      title = mimEntry.getMimEntry_title();
   	
-  	if (mimEntry.getMimEntry_synonyms()!=null) {
-  		for (int j=0; j<mimEntry.getMimEntry_synonyms().getMimEntry_synonyms_E().length; j++) {
-  			synonyms+=mimEntry.getMimEntry_synonyms().getMimEntry_synonyms_E()[j]+", ";
-  		}
-  	}
-  	if (clinicalSynopsis!=null) {
-  		for (int j=0; j<clinicalSynopsis.getMimIndexTerm().length; j++) {
-  			String key = clinicalSynopsis.getMimIndexTerm()[j].getMimIndexTerm_key();
-  			String terms = "";
-  			for (int k=0; k<clinicalSynopsis.getMimIndexTerm()[j].getMimIndexTerm_terms().getMimIndexTerm_terms_E().length; k++) {
-  				terms+=clinicalSynopsis.getMimIndexTerm()[j].getMimIndexTerm_terms().getMimIndexTerm_terms_E()[k]+", ";
-  			}
-  			synopsis+=key+": "+terms+"<br>";
-  		}
-  	}
-  	if (mimEntry.getMimEntry_summary()!=null) {
-  		for (int j=0; j<mimEntry.getMimEntry_summary().getMimText().length; j++) {
-  			summary+=mimEntry.getMimEntry_summary().getMimText()[j].getMimText_label()+": "+
-  				mimEntry.getMimEntry_summary().getMimText()[j].getMimText_text()+"<br>";
-  		}
-  	}
-  	if (mimEntry.getMimEntry_text()!=null) {
-  		for (int j=0; j<mimEntry.getMimEntry_text().getMimText().length; j++) {
-  			summary+=mimEntry.getMimEntry_text().getMimText()[j].getMimText_label()+": "+
-				mimEntry.getMimEntry_text().getMimText()[j].getMimText_text()+"<br>";
-  			//in here should create links for any omim or ref link...
-  		}  		
-  	}
-  	if (mimEntry.getMimEntry_allelicVariants()!=null) {
-  		for (int j=0; j<mimEntry.getMimEntry_allelicVariants().getMimAllelicVariant().length; j++) {
-  			allelicVariants+=mimEntry.getMimEntry_allelicVariants().getMimAllelicVariant()[j].getMimAllelicVariant_name()+
-  			" ("+mimEntry.getMimEntry_allelicVariants().getMimAllelicVariant()[j].getMimAllelicVariant_number()+")<br> ";
-  		}
-  	}
+    if (mimEntry.getMimEntry_synonyms()!=null) {
+      for (int j=0; j<mimEntry.getMimEntry_synonyms().getMimEntry_synonyms_E().length; j++) {
+        synonyms+=mimEntry.getMimEntry_synonyms().getMimEntry_synonyms_E()[j]+", ";
+      }
+    }
+    if (clinicalSynopsis!=null) {
+      for (int j=0; j<clinicalSynopsis.getMimIndexTerm().length; j++) {
+        String key = clinicalSynopsis.getMimIndexTerm()[j].getMimIndexTerm_key();
+        String terms = "";
+        for (int k=0; k<clinicalSynopsis.getMimIndexTerm()[j].getMimIndexTerm_terms().getMimIndexTerm_terms_E().length; k++) {
+          terms+=clinicalSynopsis.getMimIndexTerm()[j].getMimIndexTerm_terms().getMimIndexTerm_terms_E()[k]+", ";
+        }
+        synopsis+=key+": "+terms+"<br>";
+      }
+    }
+    if (mimEntry.getMimEntry_summary()!=null) {
+      for (int j=0; j<mimEntry.getMimEntry_summary().getMimText().length; j++) {
+        summary+=mimEntry.getMimEntry_summary().getMimText()[j].getMimText_label()+": "+
+          mimEntry.getMimEntry_summary().getMimText()[j].getMimText_text()+"<br>";
+      }
+    }
+    if (mimEntry.getMimEntry_text()!=null) {
+      for (int j=0; j<mimEntry.getMimEntry_text().getMimText().length; j++) {
+        summary+=mimEntry.getMimEntry_text().getMimText()[j].getMimText_label()+": "+
+          mimEntry.getMimEntry_text().getMimText()[j].getMimText_text()+"<br>";
+        //in here should create links for any omim or ref link...
+      }  		
+    }
+    if (mimEntry.getMimEntry_allelicVariants()!=null) {
+      for (int j=0; j<mimEntry.getMimEntry_allelicVariants().getMimAllelicVariant().length; j++) {
+        allelicVariants+=mimEntry.getMimEntry_allelicVariants().getMimAllelicVariant()[j].getMimAllelicVariant_name()+
+          " ("+mimEntry.getMimEntry_allelicVariants().getMimAllelicVariant()[j].getMimAllelicVariant_number()+")<br> ";
+      }
+    }
 
-		citation = "<b>"+title + "("+ symbol+ ")</b><br>"+synonyms+"<br><br>Summary:<br>"+summary+"Allelic Variants:<br>"+allelicVariants+"<br>Clinical Synopsis:<br>"+synopsis;
+    citation = "<b>"+title + "("+ symbol+ ")</b><br>"+synonyms+"<br><br>Summary:<br>"+summary+"Allelic Variants:<br>"+allelicVariants+"<br>Clinical Synopsis:<br>"+synopsis;
 
-  	return citation;
+    return citation;
   } 
 }
