9aec2ca3aa943beafb4d5b149925171fd72b93d2
nomi <nomi@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2010-02-08 17:45:45 +0000

    Forgot to commit this.  (I guess no one but me is trying to compile Phenote, or someone would have complained!)

---
+++ src/java/phenote/main/OldHelpManager.java
@@ -0,0 +1,51 @@
+package phenote.main;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+
+import javax.help.HelpBroker;
+import javax.help.HelpSet;
+import javax.help.HelpSetException;
+
+import org.apache.log4j.Logger;
+
+import phenote.util.FileUtil;
+
+/** Used only by old Phenote interface */
+
+public class OldHelpManager {
+  protected static HelpBroker helpBroker;
+  protected static String helpsetPath = "doc/phenote-website/help/Phenote.hs";
+  private static String jarHelpsetPath = "help/Phenote.hs";
+  private static final Logger LOG =  Logger.getLogger(OldHelpManager.class);
+  
+  public static HelpBroker getHelpBroker() {
+    if (helpBroker == null) {
+      HelpSet hs;
+      //File docsDir = new File(helpsetPath);
+      URL url;
+      try { url = FileUtil.findUrl(helpsetPath); }
+      catch (FileNotFoundException e) { // try jar path (webstart)
+        try { 
+          LOG.info("Didnt find help at "+helpsetPath+" trying "+jarHelpsetPath);
+          url = FileUtil.findUrl(jarHelpsetPath);
+          LOG.info("Found help at "+jarHelpsetPath);
+        }
+        catch  (FileNotFoundException e2) {
+          LOG.error("phenote help not found "+e);
+          return null;
+        }
+      }
+      
+      try {
+        hs = new HelpSet(null, url); //docsDir.toURL());
+      } catch (HelpSetException ee) {
+        LOG.error("HelpSet error: " + ee.getMessage());
+        return null;
+      }
+      helpBroker = hs.createHelpBroker();
+    }
+    
+    return helpBroker;
+  }
+}
