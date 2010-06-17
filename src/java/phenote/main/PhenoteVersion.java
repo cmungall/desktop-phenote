c4abf5b8cb73175d2dcc188099221a822ac2e3b9
nomi <nomi@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2010-06-17 20:16:35 +0000

    v 1.7-b14

--- src/java/phenote/main/PhenoteVersion.java
+++ src/java/phenote/main/PhenoteVersion.java
@@ -9,7 +9,7 @@ import org.apache.log4j.Logger;
 
 public class PhenoteVersion {
 
-  private static String hardwiredDefaultVersion = "1.7-beta13";
+  private static String hardwiredDefaultVersion = "1.7-beta14";
   private static final Logger LOG = Logger.getLogger(PhenoteVersion.class);
 
   public static String versionString() {
