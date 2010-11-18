7b256890df39f15dd1b055ecd9ed772d98302f2e
nomi <nomi@24f1e3b7-2810-0410-9f77-827b88cd13a9>
2010-11-18 20:50:12 +0000

    1.8.4

--- src/java/phenote/main/PhenoteVersion.java
+++ src/java/phenote/main/PhenoteVersion.java
@@ -9,7 +9,7 @@ import org.apache.log4j.Logger;
 
 public class PhenoteVersion {
 
-  private static String hardwiredDefaultVersion = "1.8.3";
+  private static String hardwiredDefaultVersion = "1.8.4";
   private static final Logger LOG = Logger.getLogger(PhenoteVersion.class);
 
   public static String versionString() {
